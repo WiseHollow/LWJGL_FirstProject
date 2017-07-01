23dfb3a1a947d73e4d6ee2b93b0bae89a31e8389
WiseHollow <ieatlotz@gmail.com>
2017-07-01 13:53:55 -0400

    Projectile remove bug fix.

--- src/net/johnbrooks/fjg/drawables/tower/TowerCannon.java
+++ src/net/johnbrooks/fjg/drawables/tower/TowerCannon.java
@@ -55,7 +55,7 @@ public class TowerCannon extends Tower
             else
             {
                 projectileList.remove(projectile);
-                Log.info("Removed projectile.");
+                i--;
             }
         }
     }
