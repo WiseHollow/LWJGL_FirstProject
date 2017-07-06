0c70d2683efaa6d1d053152491add87ad17ca290
WiseHollow <ieatlotz@gmail.com>
2017-07-06 14:50:11 -0400

    All tiles are not build-able except for stone.

--- src/net/johnbrooks/fjg/drawables/tiles/TileType.java
+++ src/net/johnbrooks/fjg/drawables/tiles/TileType.java
@@ -7,16 +7,16 @@ import org.newdawn.slick.opengl.Texture;
  */
 public enum TileType
 {
-    GRASS(TileTexture.GRASS.getTexture(), true, 0), DIRT(TileTexture.DIRT.getTexture(), true, 1),
-    WATER(TileTexture.WATER.getTexture(), false, 2), SAND(TileTexture.SAND.getTexture(), true, 3),
+    GRASS(TileTexture.GRASS.getTexture(), false, 0), DIRT(TileTexture.DIRT.getTexture(), false, 1),
+    WATER(TileTexture.WATER.getTexture(), false, 2), SAND(TileTexture.SAND.getTexture(), false, 3),
     STONE(TileTexture.STONE.getTexture(), true, 4),
 
-    DIRT_GRASS_SIDE_LEFT(TileTexture.DIRT_GRASS_LEFT.getTexture(), true, 50), DIRT_GRASS_SIDE_RIGHT(TileTexture.DIRT_GRASS_RIGHT.getTexture(), true, 51),
-    DIRT_GRASS_SIDE_BOTTOM(TileTexture.DIRT_GRASS_BOTTOM.getTexture(), true, 52), DIRT_GRASS_SIDE_TOP(TileTexture.DIRT_GRASS_TOP.getTexture(), true, 53),
-    DIRT_GRASS_SIDE_BOTTOM_LEFT(TileTexture.DIRT_GRASS_BOTTOM_LEFT.getTexture(), true, 54), DIRT_GRASS_SIDE_BOTTOM_RIGHT(TileTexture.DIRT_GRASS_BOTTOM_RIGHT.getTexture(), true, 55),
-    DIRT_GRASS_SIDE_TOP_LEFT(TileTexture.DIRT_GRASS_TOP_LEFT.getTexture(), true, 56), DIRT_GRASS_SIDE_TOP_RIGHT(TileTexture.DIRT_GRASS_TOP_RIGHT.getTexture(), true, 57),
-    DIRT_GRASS_CORNER_A(TileTexture.DIRT_GRASS_CORNER_A.getTexture(), true, 58), DIRT_GRASS_CORNER_B(TileTexture.DIRT_GRASS_CORNER_B.getTexture(), true, 59),
-    DIRT_GRASS_CORNER_C(TileTexture.DIRT_GRASS_CORNER_C.getTexture(), true, 60), DIRT_GRASS_CORNER_D(TileTexture.DIRT_GRASS_CORNER_D.getTexture(), true, 61);
+    DIRT_GRASS_SIDE_LEFT(TileTexture.DIRT_GRASS_LEFT.getTexture(), false, 50), DIRT_GRASS_SIDE_RIGHT(TileTexture.DIRT_GRASS_RIGHT.getTexture(), false, 51),
+    DIRT_GRASS_SIDE_BOTTOM(TileTexture.DIRT_GRASS_BOTTOM.getTexture(), false, 52), DIRT_GRASS_SIDE_TOP(TileTexture.DIRT_GRASS_TOP.getTexture(), false, 53),
+    DIRT_GRASS_SIDE_BOTTOM_LEFT(TileTexture.DIRT_GRASS_BOTTOM_LEFT.getTexture(), false, 54), DIRT_GRASS_SIDE_BOTTOM_RIGHT(TileTexture.DIRT_GRASS_BOTTOM_RIGHT.getTexture(), false, 55),
+    DIRT_GRASS_SIDE_TOP_LEFT(TileTexture.DIRT_GRASS_TOP_LEFT.getTexture(), false, 56), DIRT_GRASS_SIDE_TOP_RIGHT(TileTexture.DIRT_GRASS_TOP_RIGHT.getTexture(), false, 57),
+    DIRT_GRASS_CORNER_A(TileTexture.DIRT_GRASS_CORNER_A.getTexture(), false, 58), DIRT_GRASS_CORNER_B(TileTexture.DIRT_GRASS_CORNER_B.getTexture(), false, 59),
+    DIRT_GRASS_CORNER_C(TileTexture.DIRT_GRASS_CORNER_C.getTexture(), false, 60), DIRT_GRASS_CORNER_D(TileTexture.DIRT_GRASS_CORNER_D.getTexture(), false, 61);
 
     public static TileType getTileType(final int id) throws Exception
     {
