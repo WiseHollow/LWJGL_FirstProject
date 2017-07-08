96a1f9b3cc657fc586e9eebd6068d0e2dfeeec20
WiseHollow <ieatlotz@gmail.com>
2017-07-08 13:48:14 -0400

    Starting working with the "idea" of automatically generating waves.

--- src/net/johnbrooks/fjg/drawables/entities/EnemyTemplate.java
+++ src/net/johnbrooks/fjg/drawables/entities/EnemyTemplate.java
@@ -3,14 +3,19 @@ package net.johnbrooks.fjg.drawables.entities;
 import net.johnbrooks.fjg.drawables.entities.EnemyTexture;
 import org.newdawn.slick.opengl.Texture;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by ieatl on 6/30/2017.
  */
 public enum EnemyTemplate
 {
     FLY(EnemyTexture.FLY, 5, 55),
-    MOUSE(EnemyTexture.MOUSE, 10, 45), MOUSE_FAST(EnemyTexture.MOUSE, 10, 80),
+    MOUSE(EnemyTexture.MOUSE, 10, 45),
     FROG(EnemyTexture.FROG, 20, 50),
+    MOUSE_FAST(EnemyTexture.MOUSE, 10, 80),
     LADY_BUG(EnemyTexture.LADY_BUG, 20, 75),
     BEE(EnemyTexture.BEE, 25, 80),
     SNAIL(EnemyTexture.SNAIL, 100, 20),
@@ -20,9 +25,22 @@ public enum EnemyTemplate
     GHOST(EnemyTexture.GHOST, 100, 35),
     SPINNER(EnemyTexture.SPINNER, 300, 60);
 
+    /**
+     * Returns an array of enemy templates with the size of 20. Each template
+     * will have enemies
+     * @param initial
+     * @return
+     */
+    public static EnemyTemplate[] generateWavePackage(float initial)
+    {
+        EnemyTemplate[] enemyTemplates = new EnemyTemplate[20];
+        //TODO: Everything
+        return enemyTemplates;
+    }
+
     private Texture texture, altTexture, deadTexture;
     private int health;
-    private float speed;
+    private float speed, rating;
 
     EnemyTemplate(EnemyTexture enemyTexture, int health, float speed)
     {
@@ -31,6 +49,8 @@ public enum EnemyTemplate
         this.deadTexture = enemyTexture.getTextureDead();
         this.health = health;
         this.speed = speed;
+
+        this.rating = health + speed; //TODO: work on this.
     }
 
     public Texture getTexture()
@@ -47,6 +67,8 @@ public enum EnemyTemplate
     {
         return speed;
     }
+
+    public float getRating() { return rating; }
 
     public Texture getAltTexture()
     {
